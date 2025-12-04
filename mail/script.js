ab277adea2d6e0159a84d8219ba95ada409189ef
colacedavide <colace.davide00@gmail.com>
2025-12-04 14:50:41 +0100

    first commit

---
+++ mail/script.js
@@ -0,0 +1,8 @@
+// creo un array di e-mail.
+// faccio un prompt per chiedere l'email all'utente
+// faccio una verifica e-mail
+// stampo il messaggio
+
+const lista = ["davide00@gmail.com", "rick@libero.it", "ester@hotmail.it", "fabio@yahoo.com"];
+
+let email = prompt("Inserisci la tua e-mail ");
